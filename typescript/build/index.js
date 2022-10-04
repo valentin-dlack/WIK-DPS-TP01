b75befcda2fe5c86a4573ddbaeebbe10864f784a
VDAUTREMENT <phuss.pro@gmail.com>
2022-10-04 12:37:28 +0200

    express project v1, rust builded

---
+++ typescript/build/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = require("express")();
+const port = process.env.PORT || '3000'; //use port 3000 unless there exists a preconfigured port
+function TestError(test) {
+    if (test === true)
+        return false;
+    throw new Error();
+}
+app.set("json spaces", 2);
+app.get("/ping", (req, res, next) => {
+    res.json(req.headers);
+    /*test the error handling :
+      try {
+          TestError(false);
+      } catch (e) {
+          next(e);
+      }*/
+});
+app.get('*', (req, res) => {
+    res.status(404).json({ code: 404 });
+});
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ code: 500 });
+});
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ typescript/src/index.ts
@@ -0,0 +1,39 @@
+import { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = require("express")();
+const port: string = process.env.PORT || '3000'; //use port 3000 unless there exists a preconfigured port
+
+function TestError(test: boolean): boolean | never {
+    if (test === true)
+      return false;
+    throw new Error();
+}
+
+app.set("json spaces", 2);
+
+app.get("/ping", (req: Request, res: Response, next: Function) => {
+  res.json(req.headers);
+  /*test the error handling :
+    try {
+        TestError(false);
+    } catch (e) {
+        next(e);
+    }*/
+});
+
+app.get('*', (req: Request, res: Response) => {
+    res.status(404).json({ code: 404 });
+});
+
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+    console.error(err.stack);
+    res.status(500).json({ code: 500 });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+});
+
